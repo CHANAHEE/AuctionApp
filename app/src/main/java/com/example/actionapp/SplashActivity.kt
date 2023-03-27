80470f443a79a16395a77f9987c8a48e91ee1579
CHANAHEE <tjdrjs0803@naver.com>
2023-03-27 09:44:15 +0900

    First Commit

--- app/src/main/java/com/example/actionapp/SplashActivity.kt
+++ app/src/main/java/com/example/actionapp/SplashActivity.kt
@@ -3,11 +3,9 @@ package com.example.actionapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        println("Hello")
+        setContentView(R.layout.activity_splash)
     }
 }
