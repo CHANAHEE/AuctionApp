7b9f5a56719571a7cd563eb7714ed562c401ce74
CHANAHEE <tjdrjs0803@naver.com>
2023-03-28 13:36:22 +0900

    ChattingActivity 생성

---
+++ app/src/main/java/com/example/actionapp/activities/ChattingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.actionapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.actionapp.R
+
+class ChattingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}
