fdd1bd28289616fdd67f6169c403c940313ca4e9
CHANAHEE <tjdrjs0803@naver.com>
2023-03-28 16:30:06 +0900

    MainActivity BottomNavigation 아이템 선택 기능구현

--- app/src/main/java/com/example/actionapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/actionapp/activities/MainActivity.kt
@@ -2,41 +2,58 @@ package com.example.actionapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import com.example.actionapp.fragments.HomeFragment
 import com.example.actionapp.R
 import com.example.actionapp.databinding.ActivityMainBinding
+import com.example.actionapp.fragments.AuctionFragment
+import com.example.actionapp.fragments.ChatFragment
+import com.example.actionapp.fragments.CommunityFragment
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
     val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tran:FragmentTransaction = supportFragmentManager.beginTransaction()
+        setContentView(binding.root)
+
+        HomeFragment()
+        CommunityFragment()
+        AuctionFragment()
+        ChatFragment()
 
 
+        var tran:FragmentTransaction = supportFragmentManager.beginTransaction().add(R.id.container_fragment,HomeFragment())
+        tran.commit()
 
-        binding.bnv.setOnClickListener {changeFragment(it,tran)}
+        /*
+        *       BottomNavigationView 선택
+        * */
+        binding.bnv.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
+            tran = supportFragmentManager.beginTransaction()
+            changeFragment(it,tran)
+            return@OnItemSelectedListener true
+        })
+
     }
 
-    fun changeFragment(it:View,tran:FragmentTransaction){
-        when(it.id){
-            R.id.home_tab ->{
-                tran.add(R.id.container_fragment, HomeFragment()).commit()
+    
+    
+    /*
+    *       프래그먼트 전환 함수
+    * */
+    fun changeFragment(item:MenuItem,tran:FragmentTransaction){
 
-            }
-            R.id.community_tab ->{
-                tran.add(R.id.container_fragment, HomeFragment()).commit()
-            }
-            R.id.auction_tab ->{
-                tran.add(R.id.container_fragment, HomeFragment()).commit()
-            }
-            R.id.chat_tab ->{
-                tran.add(R.id.container_fragment, HomeFragment()).commit()
-            }
-
+        when(item.itemId){
+            R.id.home_tab -> tran.replace(R.id.container_fragment, HomeFragment()).commit()
+            R.id.community_tab -> tran.replace(R.id.container_fragment, CommunityFragment()).commit()
+            R.id.auction_tab -> tran.replace(R.id.container_fragment, AuctionFragment()).commit()
+            R.id.chat_tab -> tran.replace(R.id.container_fragment, ChatFragment()).commit()
         }
     }
 }
