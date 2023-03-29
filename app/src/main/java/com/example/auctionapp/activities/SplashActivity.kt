33d99c3aec9ef73627655da484f994ad65f0cf3b
CHANAHEE <tjdrjs0803@naver.com>
2023-03-29 15:30:55 +0900

    전체 layout 완료

--- app/src/main/java/com/example/auctionapp/activities/SplashActivity.kt
+++ app/src/main/java/com/example/auctionapp/activities/SplashActivity.kt
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import androidx.core.os.postDelayed
+import com.bumptech.glide.Glide
 import com.example.auctionapp.R
+import com.example.auctionapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        var ivLoading : ImageView = findViewById(R.id.iv_loading)
+        Glide.with(this).load(R.drawable.loadingicon).into(ivLoading)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this,LoginActivity::class.java))
