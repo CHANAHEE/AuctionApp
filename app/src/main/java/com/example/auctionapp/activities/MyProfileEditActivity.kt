a4ac814f2ef31764e408cc8f0a53fabbf61bd678
CHANAHEE <tjdrjs0803@naver.com>
2023-04-02 18:10:35 +0900

    ProfileEdit 완료 시 확인/취소 다이얼로그 구현

--- app/src/main/java/com/example/auctionapp/activities/MyProfileEditActivity.kt
+++ app/src/main/java/com/example/auctionapp/activities/MyProfileEditActivity.kt
@@ -1,9 +1,11 @@
 package com.example.auctionapp.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -25,6 +27,7 @@ class MyProfileEditActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        Log.i("alertdl","다이얼로그 띄우기")
         binding.btnComplete.setOnClickListener { clickCompleteBtn() }
         binding.civProfile.setOnClickListener { clickProfileImage() }
         binding.etNickname.setText("CHA")
@@ -53,11 +56,28 @@ class MyProfileEditActivity : AppCompatActivity() {
         *       변경된 프로필 정보 DB 에 저장.
         *       G 클래스의 profileImage 를 DB 에 저장하자.
         * */
+        Log.i("ale","다이얼로그 띄우기1")
         if(binding.etNickname.text.length < 3){
+            Log.i("alertdl","다이얼로그 띄우기2")
             AlertDialog.Builder(this).setMessage("닉네임을 3글자 이상으로 설정해주세요").show()
             return
+        }else {
+            Log.i("alertdl","다이얼로그 띄우기3")
+            var dialog = AlertDialog.Builder(this).setMessage("프로필을 변경하시겠습니까?").setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialog, which ->  
+                    
+                    /*
+                    *       변경된 프로필 정보 DB 저장
+                    * */
+
+                    finish()
+                }).setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->  }).create()
+
+            dialog.show()
+            dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(resources.getColor(R.color.brand,theme))
+            dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(resources.getColor(R.color.brand,theme))
         }
-        finish()
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
